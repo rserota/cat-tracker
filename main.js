afbad76a4f4397ea4d4e594e4543c4bb945a919f
Raphael Serota <raphael.serota@gmail.com>
2021-01-27 21:53:30 -0600

    cats in your inbox

--- main.js
+++ main.js
@@ -1,29 +1,72 @@
-let axios = require('axios')
+const axios = require('axios')
 const cheerio = require('cheerio');
+const nodemailer = require('nodemailer')
+const sesTransport = require('nodemailer-ses-transport');
+
+var SESCREDENTIALS = {
+  accessKeyId: "AKIA5QWXT7S354K4NAWF",
+  secretAccessKey: "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"
+};
+
+var transporter = nodemailer.createTransport(sesTransport({
+  accessKeyId: SESCREDENTIALS.accessKeyId,
+  secretAccessKey: SESCREDENTIALS.secretAccessKey,
+  rateLimit: 5
+}));
+
 
 
 const main = async ()=>{
+
+
+
 	let data = await axios.get('https://www.pawschicago.org/our-work/pet-adoption/pets-available/?tx_pawspets_pi1%5Bcatage%5D=1&cHash=f8264aa46eec7d02c56284f60e3c6076')
-	console.log('data? ', Object.keys(data))
 	const $ = cheerio.load(data.data)
 	let catLinks = $('.cats .adopt-pet a')
 	let urls = []
+	let notableCats = []
 	catLinks.each(function(i, cat){
-		console.log('cat? ', cat.attribs.href)
 		let urlParts = cat.attribs.href.split('/')
-		console.log('cat? ', urlParts)
 		catName = urlParts[urlParts.length-2] 
 		urls.push({url:cat.attribs.href, catName})
 	})
+
+	let promises = []
 	for ( let url of urls ) {
-		axios.get(url.url).then(function(data){
+		promises.push(axios.get(url.url).then(function(data){
 			const $$ = cheerio.load(data.data)
 			let catBreed = $$('.breed-cat p').text()
-			//console.log('breed? ', catBreed)
+			let catPicSrc = $$('.img-full')[0].attribs.src
 			if ( catBreed.match(/Long|Medium/) ){
 				console.log(`${url.catName}: ${catBreed}`)
+				notableCats.push({...url, catBreed, catPicSrc})
 			}
-		})
+		}))
+	}
+
+	await Promise.all(promises)
+	console.log('notable cats? ', notableCats)
+	if ( notableCats.length ) {
+
+	var mailOptions = {
+		from: 'catdad@frivolous.biz',
+		to: ['raphael.serota@gmail.com', 'tetelbenton@gmail.com'] // list of receivers
+		subject: 'Your daily cats!', // Subject line
+		html: notableCats.map(function(cat){
+			return `
+				<div>
+					<a href="${cat.url}"><p>${cat.catName} - ${cat.catBreed}</p></a>
+					<img src="${cat.catPicSrc}"
+					<hr>
+				</div>
+
+			`
+		}).join('')
+	};
+
+		// send mail with defined transport object
+		transporter.sendMail(mailOptions, function(error, info) { if (error) { console.log(error); } else { console.log('Message sent: ', info); } });
+
 	}
 }
 main()
